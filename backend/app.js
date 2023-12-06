42013aea82e634b075e3f5e9e8a2d25cc4af6ad2
ichemakina <themackinaira@yandex.ru>
2023-12-06 20:34:52 +0300

    Краш-тест

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateRegister, createUser);
 
